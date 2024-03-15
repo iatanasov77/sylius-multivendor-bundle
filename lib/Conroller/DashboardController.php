7ea7abf380d454eaf7ba159ac8ceba52d4ecafe8
Ivan I. Atanasov <i.atanasov77@gmail.com>
2024-03-15 19:23:30 +0000

    Create Vendor Dashboard.

---
+++ lib/Conroller/DashboardController.php
@@ -0,0 +1,32 @@
+<?php namespace Vankosoft\SyliusMultiVendor\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+use Doctrine\Persistence\ManagerRegistry;
+use Sylius\Component\Resource\Repository\RepositoryInterface;
+
+class DashboardController extends AbstractController
+{
+    /** @var ManagerRegistry */
+    protected $doctrine;
+    
+    /** @var RepositoryInterface */
+    protected $productRepository;
+    
+    public function __construct(
+        ManagerRegistry $doctrine,
+        RepositoryInterface $vendorsRepository
+    ) {
+        $this->doctrine             = $doctrine;
+        $this->vendorsRepository    = $vendorsRepository;
+    }
+    
+    public function index( Request $request ): Response
+    {
+        return $this->render( '@SyliusMultiVendor/Pages/Dashboard/index.html.twig', [
+            
+        ]);
+    }
+}
