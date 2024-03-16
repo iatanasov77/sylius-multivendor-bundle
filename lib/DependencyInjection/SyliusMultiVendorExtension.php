a263a4a7b4e989a76d4483a6025089d52097d8b1
Ivan I. Atanasov <i.atanasov77@gmail.com>
2024-03-16 18:25:56 +0000

    Add Doctrine Migration.

--- lib/DependencyInjection/SyliusMultiVendorExtension.php
+++ lib/DependencyInjection/SyliusMultiVendorExtension.php
@@ -2,16 +2,21 @@
 
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use Sylius\Bundle\CoreBundle\DependencyInjection\PrependDoctrineMigrationsTrait;
 
 /**
  * This is the class that loads and manages your bundle configuration
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class SyliusMultiVendorExtension extends Extension
+class SyliusMultiVendorExtension extends Extension implements PrependExtensionInterface
 {
+    use PrependDoctrineMigrationsTrait;
+    
     /**
      * {@inheritDoc}
      */
@@ -21,5 +26,32 @@ class SyliusMultiVendorExtension extends Extension
         
         $loader = new Loader\YamlFileLoader( $container, new FileLocator( __DIR__ . '/../Resources/config' ) );
         $loader->load( 'services.yaml' );
+    }
+    
+    public function getConfiguration( array $config, ContainerBuilder $container ): ConfigurationInterface
+    {
+        return new Configuration();
+    }
+    
+    public function prepend( ContainerBuilder $container ): void
+    {
+        $this->prependDoctrineMigrations($container);
+    }
+    
+    protected function getMigrationsNamespace(): string
+    {
+        return 'Vankosoft\SyliusMultiVendor\DoctrineMigrations';
+    }
+    
+    protected function getMigrationsDirectory(): string
+    {
+        return '@SyliusMultiVendorBundle/DoctrineMigrations';
+    }
+    
+    protected function getNamespacesOfMigrationsExecutedBefore(): array
+    {
+        return [
+            'Odiseo\SyliusVendorPlugin\Migrations',
+        ];
     }
 }
